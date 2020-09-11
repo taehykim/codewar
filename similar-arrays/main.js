04d0e5e0ce2a176ae16930747823c688b69fb753
Tia Kim <tiarekim@gmail.com>
2020-09-11 12:56:08 -0700

    wrote areSimilar function

--- similar-arrays/main.js
+++ similar-arrays/main.js
@@ -1,22 +1,14 @@
 function areSimilar(a, b) {
-  if (areSame(a, b)) return true;
-  for (let i = 0; i < b.length; i++) {
-    const temp = b[i];
-    for (let j = i + 1; j < b.length; j++) {
-      b[i] = b[j];
-      b[j] = temp;
-      if (areSame(a, b)) return true;
-      const temp2 = b[i];
-      b[i] = temp;
-      b[j] = temp2;
-    }
+  let sameCount = 0;
+  const diffElms = [];
+  for (let i = 0; i < a.length; i++) {
+    if (b[i] === a[i]) sameCount++;
+    else diffElms.push(a[i], b[i]);
   }
-  return false;
-}
 
-function areSame(a1, a2) {
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) return false;
-  }
+  const unique = [...new Set(diffElms)];
+
+  if (sameCount < a.length - 2) return false;
+  if (unique.length > 2) return false;
   return true;
 }
